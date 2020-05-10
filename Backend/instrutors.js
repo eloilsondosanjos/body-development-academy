89bcc3b6aabc52dc1ab8e0951988f57ffa1bcf07
eloilsonRocha <eloilson.ageedu@gmail.com>
2020-05-10 13:49:44 -0300

    separado os dados salvos em arquivodata.json temporário

--- Backend/instrutors.js
+++ Backend/instrutors.js
@@ -1,3 +1,6 @@
+const fs = require("fs");
+const data = require("./data.json");
+
 exports.post = function (req, res) {
   const keys = Object.keys(req.body);
 
@@ -7,5 +10,13 @@ exports.post = function (req, res) {
     }
   }
 
-  return res.send(req.body);
+  data.instructors.push(req.body);
+
+  fs.writeFile("data.json", JSON.stringify(data, null, 2), function (err) {
+    if (err) return res.send("Write file error!");
+
+    return res.redirect("/instructors");
+  });
+
+  //return res.send(req.body);
 };
