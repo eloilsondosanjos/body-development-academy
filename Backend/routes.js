abfc3a2550d0c59bb285eda1539440a600b8d5dd
eloilsonRocha <eloilson.ageedu@gmail.com>
2020-05-10 13:17:11 -0300

    separado as function de routes paraarquivos js

--- Backend/routes.js
+++ Backend/routes.js
@@ -1,12 +1,21 @@
 const express = require("./node_modules/express");
 const routes = express.Router();
+const instructors = require("./instrutors");
 
 routes.get("/", function (req, res) {
   return res.redirect("/instructors");
 });
+
 routes.get("/instructors", function (req, res) {
   return res.render("instructors/index.njk");
 });
+
+routes.get("/instructors/create", function (req, res) {
+  return res.render("instructors/create.njk");
+});
+
+routes.post("/instructors", instructors.post);
+
 routes.get("/members", function (req, res) {
   return res.send("members");
 });
