89cf31392fa537bf6e1e5f80160406bd69b8d19d
eloilsonRocha <eloilson.ageedu@gmail.com>
2020-05-20 22:18:41 -0300

    Implementado a logica de paginação no bfrontend

--- public/scripts.js
+++ public/scripts.js
@@ -6,3 +6,31 @@ for (item of menuItems) {
     item.classList.add("active");
   }
 }
+
+// Paginação
+
+let totalPages = 20,
+  selectedPages = 5,
+  pages = [],
+  oldPage;
+
+for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
+  const firtsAndLastPage = currentPage == 1 || currentPage == totalPages;
+  const pagesAfterSelectedPage = currentPage <= selectedPages + 2;
+  const pagesBeforeSelectedPage = currentPage >= selectedPages - 2;
+
+  if (firtsAndLastPage || (pagesBeforeSelectedPage && pagesAfterSelectedPage)) {
+    if (oldPage && currentPage - oldPage > 2) {
+      pages.push("...");
+    }
+
+    if (oldPage && currentPage - oldPage == 2) {
+      pages.push(oldPage + 1);
+    }
+    pages.push(currentPage);
+
+    oldPage = currentPage;
+  }
+}
+
+console.log(pages);
